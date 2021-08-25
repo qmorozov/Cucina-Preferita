f12f2e8026251befd73c25f7ffd0b757174a9ea0
qamorozov <andr.morozov96@gmail.com>
2021-08-25 11:17:44 +0300

    bug fix

--- src/js/main.js
+++ src/js/main.js
@@ -296,4 +296,21 @@ window.onload = function () {
         once: true,
     });
 
+    // === ANCHORS ===
+
+    const anchors = document.querySelectorAll('a[href*="#"]');
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            const blockID = anchor.getAttribute('href');
+
+            document.querySelector('' + blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
+
 };
